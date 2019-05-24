df2bc9f12ab33f3eaf6462e0a778b0669ba5dd82
Ivan <machuk.ivan@yandex.by>
2019-05-24 14:16:00 +0300

    Added shouldCompUpd to Modal

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import Aux from '../../../hoc/Auxiliary';
-import Button from '../../UI/Button/Button';
+import Aux from "../../../hoc/Auxiliary/Auxiliary";
+import Button from "../../UI/Button/Button";
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(igKey => {
-           return (
-               <li key={igKey}>
-                   <span style={{textTransform: "capitalize"}}>{igKey}</span>: {props.ingredients[igKey]}
-               </li>
-            )
-        });
+const orderSummary = props => {
+    const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
+        return (
+            <li key={igKey}>
+                <span style={{ textTransform: "capitalize" }}>{igKey}</span>:
+                {props.ingredients[igKey]}
+            </li>
+        );});
 
-    // <li>Salad: 1</li>
-    return(
+    return (
         <Aux>
             <h3>Your Order</h3>
             <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price}</strong></p>
+            <ul>{ingredientSummary}</ul>
+            <p>
+                <strong>Total Price: {props.price}</strong>
+            </p>
             <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+            <Button btnType="Danger" clicked={props.purchaseCancelled}>
+                CANCEL
+            </Button>
+            <Button btnType="Success" clicked={props.purchaseContinued}>
+                CONTINUE
+            </Button>
         </Aux>
     );
 };
