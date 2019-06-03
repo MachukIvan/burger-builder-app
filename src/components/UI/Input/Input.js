33a528d3668a1ef16bc6223866e8bd2e0e0e45ec
Ivan <machuk.ivan@yandex.by>
2019-06-03 12:53:54 +0300

    Added custom form validation

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,31 +1,45 @@
-import React from 'react';
+import React from "react";
 
-import classes from './Input.css';
+import classes from "./Input.css";
 
-const input = (props) => {
+const input = props => {
     let inputElement = null;
+    const inputClasses = [classes.InputElement];
+    let validationError = null;
+
+    if (props.invalid && props.shouldValidate && props.touched) {
+        inputClasses.push(classes.Invalid);
+        validationError = <p className={classes.ValidationError}>Please enter a valid value!</p>;
+    }
 
     switch (props.elementType) {
-        case ('input'): 
-            inputElement = <input 
-                className={classes.InputElement} 
-                {...props.elementConfig}
-                value={props.value}
-                onChange={props.changed} />
+        case "input":
+            inputElement = (
+                <input
+                    className={inputClasses.join(" ")}
+                    {...props.elementConfig}
+                    value={props.value}
+                    onChange={props.changed}
+                />
+            );
             break;
-        case ('textarea'):
-            inputElement = <textarea 
-                className={classes.InputElement} 
-                {...props.elementConfig} 
-                value={props.value}
-                onChange={props.changed} />
+        case "textarea":
+            inputElement = (
+                <textarea
+                    className={inputClasses.join(" ")}
+                    {...props.elementConfig}
+                    value={props.value}
+                    onChange={props.changed}
+                />
+            );
             break;
-        case ('select'):
+        case "select":
             inputElement = (
                 <select
-                    className={classes.InputElement}
+                    className={inputClasses.join(" ")}
                     value={props.value}
-                    onChange={props.changed} >
+                    onChange={props.changed}
+                >
                     {props.elementConfig.options.map(option => (
                         <option key={option.value} value={option.value}>
                             {option.displayValue}
@@ -34,20 +48,24 @@ const input = (props) => {
                 </select>
             );
             break;
-        default: 
-            inputElement = <input 
-                className={classes.InputElement} 
-                {...props.elementConfig}
-                value={props.value} 
-                onChange={props.changed} />
+        default:
+            inputElement = (
+                <input
+                    className={inputClasses.join(" ")}
+                    {...props.elementConfig}
+                    value={props.value}
+                    onChange={props.changed}
+                />
+            );
     }
 
     return (
         <div className={classes.Input}>
             <label className={classes.Label}>{props.label}</label>
             {inputElement}
+            {validationError}
         </div>
-    )
-}
+    );
+};
 
 export default input;
