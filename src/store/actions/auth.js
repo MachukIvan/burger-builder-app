323a24fae08856e864171cf63da4ac980099b857
Ivan <machuk.ivan@yandex.by>
2019-06-09 18:08:04 +0300

    Added Sign-In, configured storing token in redux

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -8,10 +8,11 @@ export const authStart = () => {
     };
 };
 
-export const authSuccess = authData => {
+export const authSuccess = (idToken, userId) => {
     return {
         type: actionTypes.AUTH_SUCCESS,
-        authData: authData
+        idToken: idToken,
+        userId: userId
     };
 };
 
@@ -22,7 +23,7 @@ export const authFail = error => {
     };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignUp) => {
     return dispatch => {
         dispatch(authStart());
         const authData = {
@@ -30,18 +31,17 @@ export const auth = (email, password) => {
             password: password,
             returnSecureToken: true
         };
-        axios
-            .post(
-                "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBXQge_9zWgysn0LNL3iZgmKJAuzzZVm_s",
-                authData
-            )
+        let url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyBXQge_9zWgysn0LNL3iZgmKJAuzzZVm_s";
+        if (!isSignUp) {
+            url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyBXQge_9zWgysn0LNL3iZgmKJAuzzZVm_s";
+        }
+        axios.post(url, authData)
             .then(response => {
                 console.log(response);
-                dispatch(authSuccess(response));
+                dispatch(authSuccess(response.data.idToken, response.data.localId));
             })
             .catch(err => {
-                console.log(err);
-                dispatch(authFail(err));
+                dispatch(authFail(err.response.data.error));
             });
     };
 };
