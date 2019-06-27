63b5f5de78c8d2b9f7d49a8f3f20f1cceda97949
Ivan Machuk <machuk.ivan@yandex.by>
2019-06-27 17:25:09 +0300

    Set up fetching orders via saga

--- src/store/sagas/burgerBuilder.js
+++ src/store/sagas/burgerBuilder.js
@@ -3,7 +3,7 @@ import { put } from "redux-saga/effects";
 import * as actions from "../actions/index";
 import axios from '../../axios-orders';
 
-export function* initIngredientsSaga() {
+export function* initIngredientsSaga(action) {
     try {
         const response = yield axios.get("https://react-burger-builder-a2dea.firebaseio.com/ingredients.json")
         yield put(actions.setIngredients(response.data))
